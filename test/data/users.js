5e2e424fc8f4b8ddd6d3d5b6ebf0372e752a6568
Luis Ochoa <luis.ochoa@wolox.co>
2021-04-08 10:03:48 -0500

    adding tests to the signup endpoint en refactoring the validations on the controller

--- test/data/users.js
+++ test/data/users.js
@@ -11,3 +11,16 @@ exports.repeatedUser = {
   email: 'luis.ochoa@wolox.co',
   password: 'luis1023123'
 }
+
+exports.passwordUser = {
+  firstName: 'luis',
+  lastName: 'ochoa',
+  email: 'luis.ochoa@wolox.co',
+  password: 'luis10'
+}
+
+exports.missingUser = {
+  lastName: 'ochoa',
+  email: 'luis.ochoa@wolox.co',
+  password: 'luis1asda0'
+}
--- test/users.test.js
+++ test/users.test.js
@@ -1,14 +1,10 @@
 const request = require('supertest');
 const truncateDatabase = require('./setup');
 const app = require('../app');
-const { user, repeatedUser } = require('../test/data/users');
+const { user, repeatedUser, passwordUser, missingUser } = require('../test/data/users');
 
 beforeAll(async () => {
   await truncateDatabase();
-  response = await request(app)
-        .post('/users')
-        .set('Accept', 'application/json')
-        .send(repeatedUser);
 });
 
 describe('POST /users - Signup', () => {
@@ -40,25 +36,91 @@ describe('POST /users - Signup', () => {
 describe('POST /users - Signup Failed - Repeated User', () => {
     let response;
     let status;
-    let lastName;
+    let message;
+    let response2;
     beforeAll(async () => {
         response = await request(app)
           .post('/users')
           .set('Accept', 'application/json')
           .send(repeatedUser);
+
+          response2 = await request(app)
+          .post('/users')
+          .set('Accept', 'application/json')
+          .send(repeatedUser);
   
         ({
-          body: { lastName },
+          body: { message },
+          status
+        } = response2);
+      
+    });
+  
+    it('Should signUp a new User successfully', () => {
+      expect(message).toBe('Trying to create a user that already exists');
+    });
+  
+    it('Should return status code 409', () => {
+      expect(status).toBe(409);
+    });
+  });
+
+  describe('POST /users - Password restriction', () => {
+    let response;
+    let status;
+    let message;
+    beforeAll(async () => {
+        response = await request(app)
+          .post('/users')
+          .set('Accept', 'application/json')
+          .send(passwordUser);
+  
+        ({
+          body: { message, internal_code },
           status
         } = response);
       
     });
   
     it('Should signUp a new User successfully', () => {
-      expect(lastName).toBe(repeatedUser.lastName);
+      expect(message).toBe('Password should have 8 characters minimum and only alphanumeric characters');
     });
   
     it('Should return status code 400', () => {
       expect(status).toBe(400);
+    });
+
+    it('Should return status code 400', () => {
+      expect(internal_code).toBe('bad_request_error');
+    });
+  });
+
+  describe('POST /users - Missing attribute', () => {
+    let response;
+    let status;
+    let message;
+    beforeAll(async () => {
+        response = await request(app)
+          .post('/users')
+          .set('Accept', 'application/json')
+          .send(missingUser);
+  
+        ({
+          body: { message, internal_code },
+          status
+        } = response);
+      
+    });
+  
+    it('Should signUp a new User successfully', () => {
+      expect(message).toBe('There are missing fields');
+    });
+  
+    it('Should return status code 400', () => {
+      expect(status).toBe(400);
+    });
+
+    it('Should return status code 400', () => {
+      expect(internal_code).toBe('bad_request_error');
     });
   });
